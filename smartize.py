031a75119d8a2983bf082ee9861dccc47844985f
Blake Garretson <blake@blakeg.net>
2014-07-17 15:39:59 -0400

    Added unittest suite. Added case to handle years like '90 or '90s.

--- smartize.py
+++ smartize.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re 
+import re, unittest
 
 def smartize(text, grave=False, markup=False):
     # quotes
@@ -21,26 +21,32 @@ def smartize(text, grave=False, markup=False):
         close_single = "’"
     
     if grave:
-        text = text.replace("```", open_double_single)    
-        text = text.replace("'''", close_single_double)
-        text = text.replace("``", open_double)    
-        text = text.replace("`", open_single)
-        text = text.replace("''", close_double)
-        text = text.replace("'", close_single)
+        re_triple_open = re.compile(r"""(?i)(```)(?=[a-z0-9])""")
+        re_triple_close = re.compile(r"""(?i)(?<=[a-z0-9.!?%])(''')""")
+        re_double_open = re.compile(r'(?i)(``)(?=[a-z0-9])')
+        re_double_close = re.compile(r"(?i)(?<=[a-z0-9.!?%])('')")
+        re_single_close = re.compile(r"(?i)((?<=[a-z0-9.!?%])')|((?<=\s)'(?=(tis\b|twas\b)))|((?<=\s)'(?=[0-9]+s*\b))")
+        re_single_open = re.compile(r"(?i)((?<![a-z])(`)(?=[a-z0-9]))|((?<=\s)'(?=[0-9]))")
+        # text = text.replace("```", open_double_single)    
+        # text = text.replace("'''", close_single_double)
+        # text = text.replace("``", open_double)    
+        # text = text.replace("`", open_single)
+        # text = text.replace("''", close_double)
+        # text = text.replace("'", close_single)
     else:
         re_triple_open = re.compile(r"""(?i)("')(?=[a-z0-9])""")
         re_triple_close = re.compile(r"""(?i)(?<=[a-z0-9.!?%])('")""")
         re_double_open = re.compile(r'(?i)(")(?=[a-z0-9])')
         re_double_close = re.compile(r'(?i)(?<=[a-z0-9.!?%])(")')
-        re_single_close = re.compile(r"(?i)((?<=[a-z0-9.!?%])')|((?<=\s)'(?=(tis\b|twas\b)))")
+        re_single_close = re.compile(r"(?i)((?<=[a-z0-9.!?%])')|((?<=\s)'(?=(tis\b|twas\b)))|((?<=\s)'(?=[0-9]+s*\b))")
         re_single_open = re.compile(r"(?i)(?<![a-z])(')(?=[a-z0-9])")
 
-        text = re_triple_open.sub(open_double_single, text)  
-        text = re_triple_close.sub(close_single_double, text)
-        text = re_double_open.sub(open_double, text)
-        text = re_double_close.sub(close_double, text)
-        text = re_single_close.sub(close_single, text)
-        text = re_single_open.sub(open_single, text)
+    text = re_triple_open.sub(open_double_single, text)  
+    text = re_triple_close.sub(close_single_double, text)
+    text = re_double_open.sub(open_double, text)
+    text = re_double_close.sub(close_double, text)
+    text = re_single_close.sub(close_single, text)
+    text = re_single_open.sub(open_single, text)
     
     # other chars
     text = text.replace("...","…")
@@ -58,6 +64,85 @@ def smartize(text, grave=False, markup=False):
     
     return text
 
+class TestSequenceFunctions(unittest.TestCase):
+    """Run this test suite with: python -m unittest smartize
+    
+    If you are running the test suite on a Windows console that doesn't support
+    unicode characters (which is normally the case), then you can run "chcp 65001"
+    on the command line before running the test.  This changes the codepage so the
+    error message (if one occurs) will display the unicode characters correctly.
+    """
+    def setUp(self):
+        self.cases = (
+                        # Straight Quotes Input
+                        ('"test"','“test”', False, False),
+                        ('"test."','“test.”', False, False),
+                        ('"test!"','“test!”', False, False),
+                        ('"test?"','“test?”', False, False),
+                        ('"test%"','“test%”', False, False),
+                        ('"test" ','“test” ', False, False),
+                        (' "test"',' “test”', False, False),
+                        ("'test'",'‘test’', False, False),
+                        ("'test.'",'‘test.’', False, False),
+                        ("'test!'",'‘test!’', False, False),
+                        ("'test?'",'‘test?’', False, False),
+                        ("'test%'",'‘test%’', False, False),
+                        ("'test' ",'‘test’ ', False, False),
+                        (" 'test'",' ‘test’', False, False),
+                        ('"\'test\'"','“‘test’”', False, False),
+                        (' "\'test\'" ',' “‘test’” ', False, False),
+                        ('"\'test\' and \'test\'"','“‘test’ and ‘test’”', False, False),
+                        (" '90 ",' ’90 ', False, False),
+                        (" '90s ",' ’90s ', False, False),
+                        (" '90a "," ‘90a ", False, False),
+                        (" 'twas",' ’twas', False, False),
+                        (" 'tis",' ’tis', False, False),
+                        ("isn't",'isn’t', False, False),
+                        # Misc chars
+                        ("a---b","a—b", False, False),
+                        (" ---b"," ---b", False, False),
+                        ("a--- ","a--- ", False, False),
+                        ("---","---", False, False),
+                        ("1--2","1–2", False, False),
+                        ("1--a","1--a", False, False),                
+                        ("...","…", False, False),
+                        ("(C)","©", False, False),
+                        ("(R)","®", False, False),
+                        ("(TM)","™", False, False),
+                        # Grave Markup Quotes Input
+                        ("``test''",'“test”', True, False),
+                        ("``test.''",'“test.”', True, False),
+                        ("``test!''",'“test!”', True, False),
+                        ("``test?''",'“test?”', True, False),
+                        ("``test%''",'“test%”', True, False),
+                        ("``test'' ",'“test” ', True, False),
+                        (" ``test''",' “test”', True, False),
+                        ("`test'",'‘test’', True, False),
+                        ("`test.'",'‘test.’', True, False),
+                        ("`test!'",'‘test!’', True, False),
+                        ("`test?'",'‘test?’', True, False),
+                        ("`test%'",'‘test%’', True, False),
+                        ("`test' ",'‘test’ ', True, False),
+                        (" `test'",' ‘test’', True, False),
+                        ('```test\'\'\'','“‘test’”', True, False),
+                        (' ```test\'\'\' ',' “‘test’” ', True, False),
+                        ('```test\' and `test\'\'\'','“‘test’ and ‘test’”', True, False),
+                        (" '90 ",' ’90 ', True, False),
+                        (" '90s ",' ’90s ', True, False),
+                        (" '90a "," ‘90a ", True, False),
+                        (" 'twas",' ’twas', True, False),
+                        (" 'tis",' ’tis', True, False),
+                        ("isn't",'isn’t', True, False),
+                    )
+    def test_smartize(self):
+        for before, after, grave, markup in self.cases:
+            smartized = smartize(before, grave, markup)
+            self.assertEqual(smartized, after, 
+                            "\n Original text: >>>%s<<< (grave:%s, markup:%s)"\
+                            "\nSmartized text: >>>%s<<<"\
+                            "\n Expected text: >>>%s<<<" \
+                            % (before, grave, markup, smartized, after))
+            
 help_desc = """Convert dumb characters to smart characters.
 
 Straight quotes like " and ' are converted to curly quotes 
